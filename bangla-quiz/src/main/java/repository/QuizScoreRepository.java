cd4a97beb9776d110c487044fd2b8705f6f100f2
Rezowan Rahman Raj <ict.rezowanology@gmail.com>
2025-08-06 18:23:11 +0600

    Delete bangla-quiz directory

--- bangla-quiz/src/main/java/repository/QuizScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.banglaquiz.repository;
-
-import com.example.banglaquiz.model.QuizScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizScoreRepository extends JpaRepository<QuizScore, Integer> {
-}
